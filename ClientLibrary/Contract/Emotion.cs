7e375b4377950586da2f5addd0f8b112204d42e4
Chris Sienkiewicz <chsienki@microsoft.com>
2017-01-09 16:32:44 -0800

    Add emotion and emotionScores classes from the emotion SDK as common contracts

--- ClientLibrary/Contract/Emotion.cs
+++ ClientLibrary/Contract/Emotion.cs
@@ -39,14 +39,58 @@ using System.Threading.Tasks;
 
 namespace Microsoft.ProjectOxford.Common.Contract
 {
-    /// <summary>
-    /// Face object returned as part of the FaceDetection/EmotionRecognition operations.
-    /// </summary>
-    public class VideoFace
+    public class Emotion
     {
         /// <summary>
-        /// Gets or sets Id of face.
+        /// Gets or sets the face rectangle.
         /// </summary>
-        public int FaceId { get; set; }
+        /// <value>
+        /// The face rectangle.
+        /// </value>
+        public Rectangle FaceRectangle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the emotion scores.
+        /// </summary>
+        /// <value>
+        /// The emotion scores.
+        /// </value>
+        public EmotionScores Scores { get; set; }
+
+        #region overrides
+        public override bool Equals(object o)
+        {
+            if (o == null) return false;
+
+            var other = o as Emotion;
+
+            if (other == null) return false;
+
+            if (this.FaceRectangle == null)
+            {
+                if (other.FaceRectangle != null) return false;
+            }
+            else
+            {
+                if (!this.FaceRectangle.Equals(other.FaceRectangle)) return false;
+            }
+
+            if (this.Scores == null)
+            {
+                return other.Scores == null;
+            }
+            else
+            {
+                return this.Scores.Equals(other.Scores);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            int r = (FaceRectangle == null) ? 0x33333333 : FaceRectangle.GetHashCode();
+            int s = (Scores == null) ? 0xccccccc : Scores.GetHashCode();
+            return r ^ s;
+        }
+        #endregion
     }
 }
